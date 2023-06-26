ff0e0715d28436d2cfc71261f521f6bb10113df7
aidan.newell <acnewell09@gmail.com>
2023-06-26 07:39:12 -0500

    adding search functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
+
+import SearchManufacturer from "./SearchManufacturer";
+
+const handleSearch = () => {};
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <form onSubmit={handleSearch} className="searchbar">
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
