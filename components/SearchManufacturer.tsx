2333399dd0eb597c74093de8a4bf6a21405ed4f8
aidan.newell <acnewell09@gmail.com>
2023-06-27 08:03:52 -0500

    car manufacturer search

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, Fragment } from "react";
 import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 import { SearchManufacturerProps } from "@/types";
@@ -8,6 +9,8 @@ const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -21,8 +24,22 @@ const SearchManufacturer = ({
               alt="car logo"
             />
           </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
       </Combobox>
+
+      <Transition
+        as={Fragment}
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        afterLeave={() => setQuery("")}
+      ></Transition>
     </div>
   );
 };
