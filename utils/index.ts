b17f863881b61443ae1236695821e4197bfae7fa
aidan.newell <acnewell09@gmail.com>
2023-06-29 07:12:18 -0500

    added API fetching

--- utils/index.ts
+++ utils/index.ts
@@ -1,23 +1,17 @@
-const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "a5398b92f7msh92fe8d07f1f9534p108575jsn6ea618034835",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  },
-};
-
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
-}
-
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "a5398b92f7msh92fe8d07f1f9534p108575jsn6ea618034835",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
 }
