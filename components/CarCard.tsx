44bb63fb0e2925a127c3ade776980bfdde06b16b
aidan.newell <acnewell09@gmail.com>
2023-06-30 08:38:41 -0500

    progress on car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
+import { calculateCarRent } from "@/utils";
 
 interface CarCardProps {
   car: CarProps;
@@ -12,7 +13,23 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
-  return <div>CarCard</div>;
+
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+        <p className="flex mt-6 text-[32px] font-extrabold">
+          <span className="self-start text-[14px] font-semibold">$</span>
+          {carRent}
+          <span className="self-end text-[14px] font-medium"> /day</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
