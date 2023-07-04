647d703e571fbaf214183c45a2e8ba09150dc5a1
aidan.newell <acnewell09@gmail.com>
2023-07-04 13:10:03 -0500

    importing some stuff to make the car detail modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import Image from "next/image";
+import { Fragment } from "react";
+
+import { Dialog, Transition } from "@headlessui/react";
 
 import { CarProps } from "@/types";
 
