c2b47de725abf27d246ab96f00550b819896c882
aidan.newell <acnewell09@gmail.com>
2023-07-05 07:51:57 -0500

    car details modal base

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 import Image from "next/image";
 import { Fragment } from "react";
-
 import { Dialog, Transition } from "@headlessui/react";
 
 import { CarProps } from "@/types";
@@ -12,7 +13,17 @@ interface CarDetailsProps {
 }
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
-  return <div>CarDetails</div>;
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child>
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
 export default CarDetails;
