d1a8c281adf39e9f1c84b961ca0693dd83570238
aidan.newell <acnewell09@gmail.com>
2023-07-06 07:52:51 -0500

    building the details pop up modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -30,7 +30,45 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center"></div>
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-out duration-300"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src="/hero.png"
+                        alt="car model"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
         </Dialog>
       </Transition>
