f0506a0b81eac8d26aeb304a155bda1500239c58
aidan.newell <acnewell09@gmail.com>
2023-07-13 08:48:50 -0500

    thinkingabout how to get around the API account reg

--- utils/index.ts
+++ utils/index.ts
@@ -33,6 +33,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
+// need to figure out if this will work without the API call; can we just use static image?
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
 };
